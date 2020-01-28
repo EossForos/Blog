12037f7d558d6950fe776589b4b3e9887592c505
EossForos <volosonopulos@gmail.com>
2020-01-28 16:53:22 +0200

    User Update & upgrade post

--- django_project/settings.py
+++ django_project/settings.py
@@ -129,4 +129,4 @@ MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
-LOGIN_URL = 'loginclear'
+LOGIN_URL = 'login'
--- users/forms.py
+++ users/forms.py
@@ -3,12 +3,14 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
+
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
@@ -17,8 +19,8 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
 
+
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image', ]
-
+        fields = ['image']
